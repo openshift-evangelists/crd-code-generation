58247d9153464ef22a7696c4d63b9bfbc23708e5
Jonas Kongslund <jonas@kongslund.net>
2017-11-02 11:14:41 +0400

    Fix version: v1alpha1 -> v1

--- pkg/apis/example.com/v1/register.go
+++ pkg/apis/example.com/v1/register.go
@@ -9,7 +9,7 @@ import (
 )
 
 // SchemeGroupVersion is group version used to register these objects
-var SchemeGroupVersion = schema.GroupVersion{Group: examplecom.GroupName, Version: "v1alpha1"}
+var SchemeGroupVersion = schema.GroupVersion{Group: examplecom.GroupName, Version: "v1"}
 
 // Resource takes an unqualified resource and returns a Group qualified GroupResource
 func Resource(resource string) schema.GroupResource {
