1f9ce67f17e8a382bdabd29650e732e6eb5c4d2b
Mike Arpaia <mike@arpaia.co>
2018-02-03 19:12:27 -0700

    Updating the year in the generated code

--- pkg/client/informers/externalversions/example/interface.go
+++ pkg/client/informers/externalversions/example/interface.go
@@ -1,5 +1,5 @@
 /*
-Copyright 2017 The Openshift Evangelists
+Copyright 2018 The Openshift Evangelists
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
